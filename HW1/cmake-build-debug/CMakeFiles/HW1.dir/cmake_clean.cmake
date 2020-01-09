e49591e89d5db7b3140291ecf14247318f907e17
Uğur Ali Kaplan <uguralikaplan@gmail.com>
2020-01-09 20:47:58 +0300

    Added reports

---
+++ HW1/cmake-build-debug/CMakeFiles/HW1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HW1.dir/main.cpp.o"
+  "HW1"
+  "HW1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HW1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
